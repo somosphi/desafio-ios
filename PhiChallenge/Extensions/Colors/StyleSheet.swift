cbc77316c2461fd50940fd76422c35a42ff9cc8d
Felipe Wilke <felipew.dev@gmail.com>
2020-11-10 18:00:44 -0300

    Add  UIColors extension

---
+++ PhiChallenge/Extensions/Colors/StyleSheet.swift
@@ -0,0 +1,21 @@
+//
+//  StyleSheet.swift
+//  PhiChallenge
+//
+//  Created by Felipe Wilke on 10/11/20.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let DarkGrayHex  = UIColor(hexaRGB: "#828282")
+    static let lightGrayHex = UIColor(hexaRGB: "#f8f8f8")
+    static let blackHex     = UIColor(hexaRGB: "#202021")
+    static let cyanHex      = UIColor(hexaRGB: "#00c1af")
+    
+    
+    
+}
+
+
