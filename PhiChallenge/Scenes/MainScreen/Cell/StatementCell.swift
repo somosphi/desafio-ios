8b92477e647a8309cd5d1e62687616a84171c3b0
Felipe Wilke <felipew.dev@gmail.com>
2020-11-11 09:50:32 -0300

    Setup statement cell layout

--- PhiChallenge/Scenes/MainScreen/Cell/StatementCell.swift
+++ PhiChallenge/Scenes/MainScreen/Cell/StatementCell.swift
@@ -8,16 +8,85 @@
 import UIKit
 
 final class StatementCell: UITableViewCell {
-
+    
+    private let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGrayHex
+        
+        return view
+    }()
+    
+    private let ballView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyanHex
+        view.layer.cornerRadius = 7
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.white.cgColor
+        
+        return view
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Transferência Enviada"
+        label.textColor = .blackHex
+        label.textAlignment = .left
+        label.font = UIFont(name: Fonts.arial, size: 16)
+        
+        return label
+    }()
+    
+    private let receiverlabel: UILabel = {
+        let label = UILabel()
+        label.text = "David Blond"
+        label.textColor = .darkGrayHex
+        label.textAlignment = .left
+        label.font = UIFont(name: Fonts.arial, size: 15)
+        
+        return label
+    }()
+    
+    private let amountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "-R$ 100,00"
+        label.textColor = .blackHex
+        label.textAlignment = .left
+        label.font = UIFont(name: Fonts.arialBold, size: 16)
+        
+        return label
+    }()
+    
+    private let createAtlabel: UILabel = {
+        let label = UILabel()
+        label.text = "13/10"
+        label.textColor = .darkGrayHex
+        label.textAlignment = .center
+        label.font = UIFont(name: Fonts.arial, size: 16)
+        
+        return label
+    }()
+    
+    private let pixFlagLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pix"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont(name: Fonts.arial, size: 12)
+        label.backgroundColor = .cyanHex
+        
+        return label
+        
+    }()
+    
+    
     static let identifier = "StatementCell"
 
 //MARK: - Object lifecycle
 override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     
-   
-
-    backgroundColor = .white
+        setupViewConfiguration()
+        setup()
 }
 
 required init?(coder: NSCoder) {
