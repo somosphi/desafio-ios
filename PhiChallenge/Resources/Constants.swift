83ae177198c9cd4701e2b2ec871fd65286f9e8f7
Felipe Wilke <felipew.dev@gmail.com>
2020-11-11 09:49:15 -0300

    Add StyleSheet and Contants file

---
+++ PhiChallenge/Resources/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  PhiChallenge
+//
+//  Created by Felipe Wilke on 09/11/20.
+//
+
+import Foundation
+
+enum K {
+    static let yourTransactions = "Suas Movimentações"
+    
+}
+
+
