027e1704612dcf53a08f7a475787931000c231f0
Felipe Wilke <felipew.dev@gmail.com>
2020-11-11 09:50:48 -0300

    setup my balance header layout

--- PhiChallenge/Scenes/MainScreen/Header/MyBalanceHeader.swift
+++ PhiChallenge/Scenes/MainScreen/Header/MyBalanceHeader.swift
@@ -9,11 +9,48 @@ import UIKit
 
 final class MyBalanceHeader: UIView {
 
+    private let myBalanceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Seu Saldo"
+        label.textColor = .blackHex
+        label.textAlignment = .left
+        label.font = UIFont(name: Fonts.arial, size: 16)
+        
+        return label
+    }()
+    
+    private let amountLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont(name: Fonts.arialBold, size: 26)
+        label.text = "R$ 1345,00"
+        label.textColor = .cyanHex
+        label.isHidden = false
+        
+        return label
+    }()
+    
+    private let eyeImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.backgroundColor = .cyanHex
+        
+        return imageView
+    }()
+    
+    private let hideAmountView: UIImageView = {
+        let imageView = UIImageView()
+         imageView.backgroundColor = .cyanHex
+         imageView.isHidden = true
+         
+         return imageView
+    }()
+    
+    //MARK: -  Initialization
     
     override init(frame: CGRect) {
         super.init(frame: frame)
        
-//       setupViewConfiguration()
+       setupViewConfiguration()
         setup()
     }
     
