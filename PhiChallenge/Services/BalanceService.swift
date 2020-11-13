202c00172ae66fb7923f115243989f2c7f2a2748
Felipe Wilke <felipew.dev@gmail.com>
2020-11-13 12:39:51 -0300

    Add balance service layer

---
+++ PhiChallenge/Services/BalanceService.swift
@@ -0,0 +1,42 @@
+//
+//  MyBalanceService.swift
+//  PhiChallenge
+//
+//  Created by Felipe Wilke on 11/11/20.
+//
+
+import Foundation
+
+import Foundation
+
+final class BalanceService: BalanceServiceInput {
+    
+    weak var outputBalance: BalanceServiceOutput?
+
+    private let api: APIRequestProtocol
+    
+    init(api: APIRequestProtocol) {
+        self.api = api
+    }
+    
+    //MARK: - MyBalance API Request
+    func fetchBalance() {
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "desafio-ios-phi-bff.herokuapp.com"
+        urlComponents.path = "/myBalance"
+        
+        guard let urlString = urlComponents.url?.absoluteString else { return }
+        
+        api.request(urlString: urlString,
+                    method: .get,
+                    parameters: nil) { [weak self] (response: Balance) in
+            print(response.amount)
+            self?.outputBalance?.didUpdateBalanceSuccess(response.amount)
+            
+        } failure: { [weak self] (error) in
+            self?.outputBalance?.didUpdatBalanceFail(error)
+        }
+    }
+}
