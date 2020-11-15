db2e4ba9bf33edc7f790da93f0d64bfafa2d8083
Felipe Wilke <felipew.dev@gmail.com>
2020-11-15 03:04:57 -0300

    Add details screen router

--- PhiChallenge/Scenes/DetailsScreen/DetailsRouter.swift
+++ PhiChallenge/Scenes/DetailsScreen/DetailsRouter.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import UIKit
+
+final class DetailsRouter {
+    //MARK: - Properties
+    
+    private weak var context: UIViewController?
+    private let id: String
+    
+    //MARK: - Object Lifecycle
+    
+    init(context: UIViewController, id: String) {
+        self.context = context
+        self.id = id
+    }
+    
+    //MARK: - Public Methods
+    
+    func makeViewController() -> UIViewController {
+        
+        let api = APIRequest()
+        let service = DetailsService(api: api)
+        let presenter = DetailsPresenter(id: id, service: service)
+        let viewController = DetailsViewController(presenter: presenter)
+        
+        context = viewController
+        
+        return viewController
+    }
+}
