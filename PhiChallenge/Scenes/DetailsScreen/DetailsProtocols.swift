39d7d1de862c7e8b5d4dcaca0ed9165e21a127f3
Felipe Wilke <felipew.dev@gmail.com>
2020-11-15 03:04:47 -0300

    Add details screen protocols

--- PhiChallenge/Scenes/DetailsScreen/DetailsProtocols.swift
+++ PhiChallenge/Scenes/DetailsScreen/DetailsProtocols.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol DetailsScreenView: AnyObject {
+    func showLoader()
+    func hideLoader()
+    func showError(message: String)
+    func setAmount(with text: String)
+    func setReceiver(with text: String)
+    func setDescription(with text: String)
+    func setBankName(with text: String)
+    func setAuthentication(with text: String)
+    func setCreatedAt(with text: String)
+}
