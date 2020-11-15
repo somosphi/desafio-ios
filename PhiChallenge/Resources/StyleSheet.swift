6a991293275b28053997187658dd216950ca4c83
Felipe Wilke <felipew.dev@gmail.com>
2020-11-15 03:03:00 -0300

    Add style sheet file

--- PhiChallenge/Resources/StyleSheet.swift
+++ PhiChallenge/Resources/StyleSheet.swift
@@ -7,20 +7,23 @@
 
 import UIKit
 
+
+
+//MARK: - Application Hexadecimal colors
+
 extension UIColor {
-    
     static let darkGrayHex  = UIColor(hexaRGB: "#828282")
     static let lightGrayHex = UIColor(hexaRGB: "#f8f8f8")
     static let blackHex     = UIColor(hexaRGB: "#202021")
     static let cyanHex      = UIColor(hexaRGB: "#00c1af")
-    
 }
 
+//MARK: - Application Fonts Type
+
 enum Fonts {
-    static let arial = "ArialMT"
+    static let arialMT = "ArialMT"
     static let arialBold = "Arial-BoldMT"
+    static let KohinoorBanglaRegular = "KohinoorBangla-Regular"
     static let KohinoorBanglaSemiBold = "KohinoorBangla-Semibold"
-    
-    
 }
 
