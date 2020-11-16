8686e2e4471731ca13d576725fb810b369a440dd
Felipe Wilke <felipew.dev@gmail.com>
2020-11-16 04:30:20 -0300

    Add main screen presenter unit tests

--- PhiChallengeTests/MainScreen/MainScreenPresenterTests.swift
+++ PhiChallengeTests/MainScreen/MainScreenPresenterTests.swift
@@ -31,21 +31,20 @@ class MainScreenPresenterTests: XCTestCase {
         sut.viewDidload()
         
         XCTAssertTrue(balanceService.fetchBalanceCalled)
-        
     }
     
     func test_viewDidLoad_shouldFetchStatements() {
         sut.viewDidload()
         
         XCTAssertTrue(statementService.fetchStatementCalled)
-        XCTAssertEqual(statementService.fetchStatementPassed, "0")
+        XCTAssertEqual(statementService.fetchStatementOffsetPassed, "0")
     }
     
     func test_viewDidLoad_givenStatementServiceHasAlreadyBeenCalledBefore_shouldIncrementOffset() {
         sut.fetchStatements()
         sut.fetchStatements()
         
-        XCTAssertEqual(statementService.fetchStatementPassed, "1")
+        XCTAssertEqual(statementService.fetchStatementOffsetPassed, "1")
     }
     
     func test_tryAgain_givenAlertActionButtonHasBeenTapped_shouldCallBalanceServiceAndStatementService() {
@@ -53,13 +52,14 @@ class MainScreenPresenterTests: XCTestCase {
         
         XCTAssertTrue(balanceService.fetchBalanceCalled)
         XCTAssertTrue(statementService.fetchStatementCalled)
-        XCTAssertEqual(statementService.fetchStatementPassed, "0")
+        XCTAssertEqual(statementService.fetchStatementOffsetPassed, "0")
     }
     
     func test_didUpdateBalanceSuccess_givenDataReceived_shouldDelegateDidUpdateBalance() {
         sut.didUpdateBalanceSuccess(1325)
         
         XCTAssertTrue(view.didUpdateBalanceCalled)
+        XCTAssertEqual(view.didUpdateBalancePassed, 1325)
     }
     
     func test_didUpdateBalanceFail_shouldDelegateShowError () {
@@ -73,35 +73,30 @@ class MainScreenPresenterTests: XCTestCase {
         sut.didUpdateStatementSuccess([Items.fixture()])
         
         XCTAssertTrue(view.hideLoaderCalled)
-        
     }
     
     func test_didUpdateStatementSuccess_givendataReceived_shouldDelegateReloadTableViewData() {
         sut.didUpdateStatementSuccess([Items.fixture()])
         
         XCTAssertTrue(view.reloadTableViewDataCalled)
-        
     }
     
     func test_didUpdateStatementSuccess_givenNoDataReceived_shouldDelegateHideLoader() {
         sut.didUpdateStatementSuccess([])
         
         XCTAssertTrue(view.hideLoaderCalled)
-        
     }
     
     func test_didUpdateStatementSuccess_givenNodataReceived_shouldDelegateReloadTableViewData() {
         sut.didUpdateStatementSuccess([])
         
         XCTAssertTrue(view.reloadTableViewDataCalled)
-        
     }
     
     func test_didUpdateStatementFail_shouldDelegateHideLoader() {
         sut.didUpdateStatementFail(AppError.generic)
         
         XCTAssertTrue(view.hideLoaderCalled)
-        
     }
     
     func test_didUpdateStatementfail_shouldDelegateShowError() {
@@ -109,22 +104,20 @@ class MainScreenPresenterTests: XCTestCase {
         
         XCTAssertTrue(view.showErrorCalled)
         XCTAssertEqual(view.showErrorPassed, "Ops, something went wrong. Try again later.")
-        
     }
     
     func test_didSelectItemAt_shouldRouterTodetailsScene() {
-        sut.statements = [Items.fixture(id: "0"),
-                          Items.fixture(id: "1"),
-                          Items.fixture(id: "2"),
-                          Items.fixture(id: "3"),
-                          Items.fixture(id: "4"),
-                          Items.fixture(id: "5")]
-        
+        sut.statements = [Items.fixture(id: "0000"),
+                          Items.fixture(id: "1111"),
+                          Items.fixture(id: "2222"),
+                          Items.fixture(id: "3333"),
+                          Items.fixture(id: "4444"),
+                          Items.fixture(id: "5555")]
         
         sut.didSelectItemAt(index: 2)
         
         XCTAssertTrue(router.navigateToDetailsSceneCalled)
-        XCTAssertEqual(router.navigateToDetailsScenePassed, "2")
+        XCTAssertEqual(router.statementIdPassed, "2222")
     }
 
 }
