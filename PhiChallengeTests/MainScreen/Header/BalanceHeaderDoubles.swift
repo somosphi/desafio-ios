bd3e2414ca951e1e86aa23dd1b4c47367adb74fc
Felipe Wilke <felipew.dev@gmail.com>
2020-11-16 04:31:28 -0300

    Add balance header doubles

--- PhiChallengeTests/MainScreen/Header/BalanceHeaderDoubles.swift
+++ PhiChallengeTests/MainScreen/Header/BalanceHeaderDoubles.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+@testable import PhiChallenge
+
+final class BalanceHeaderViewSpy: BalanceHeaderView {
+    
+    private(set) var setAmountCalled = false
+    private(set) var setAmountPassed: String?
+    
+    func setAmount(with text: String) {
+        setAmountCalled = true
+        setAmountPassed = text
+    }
+    
+    private(set) var hideAmountCalled = false
+    private(set) var hideAmountFlagPassed: Bool?
+    private(set) var hideAmountImagePassed: String?
+    
+    func hideAmount(with flag: Bool, image: String) {
+        hideAmountCalled = true
+        hideAmountFlagPassed = flag
+        hideAmountImagePassed = image
+    }
+}
