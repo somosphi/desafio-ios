9c73c948146739cc1def4676fffa059e991fa5d8
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-12-05 17:33:44 -0300

    Base project and gitignore

---
+++ desafio-ios/desafio-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  desafio-ios
+//
+//  Created by Alexandre Scheer Bing on 05/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
