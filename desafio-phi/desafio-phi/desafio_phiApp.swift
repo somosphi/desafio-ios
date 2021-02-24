30c6e3d7975ce3f16031538be07081648bca073a
Marcus Vinicius Vieira Badiale <marcusbadiale@Marcuss-MacBook-Pro.local>
2021-02-24 14:50:13 -0300

    Project setup.

---
+++ desafio-phi/desafio-phi/desafio_phiApp.swift
@@ -0,0 +1,17 @@
+//
+//  desafio_phiApp.swift
+//  desafio-phi
+//
+//  Created by Marcus Vinicius Vieira Badiale on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct desafio_phiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
