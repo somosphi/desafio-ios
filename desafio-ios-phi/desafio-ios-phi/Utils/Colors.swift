add90243fe627dd3022e6a50b17e6c4f5ccb792d
Lidiane Gomes <lidianegomes@c0387.local>
2021-04-27 14:49:02 -0300

    extensão de cores

---
+++ desafio-ios-phi/desafio-ios-phi/Utils/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  desafio-ios-phi
+//
+//  Created by Lidiane Gomes on 27/04/21.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    static let grayTextColor = UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1)
+    static let blackTextColor = UIColor(red: 32/255, green: 32/255, blue: 33/255, alpha: 1)
+    static let whiteOffColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
+    static let lightGreenColor = UIColor(red: 0, green: 193/255, blue: 175/255, alpha: 1)
+}
