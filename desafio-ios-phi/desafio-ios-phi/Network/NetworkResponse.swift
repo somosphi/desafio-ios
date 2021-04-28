234361248a1bbdda3d3c6166207b01579ee1d8dc
Lidiane Gomes <lidianegomes@c0387.local>
2021-04-28 17:51:10 -0300

    respostas para erros de rede

--- desafio-ios-phi/desafio-ios-phi/Network/NetworkResponse.swift
+++ desafio-ios-phi/desafio-ios-phi/Network/NetworkResponse.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+enum NetworkResponse: String {
+    case success
+    case authenticationError = "You need to be authenticated first."
+    case badRequest = "Bad request"
+    case outdated = "The url you requested is outdated."
+    case failed = "Network request failed."
+    case noData = "Response returned with no data to decode."
+    case unableToDecode = "We could not decode the response."
+}
