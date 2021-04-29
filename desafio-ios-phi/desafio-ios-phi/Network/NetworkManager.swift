b94049c162e0950f13d46fae17098af9e6ddbf51
Lidiane Gomes <lidianegomes@c0387.local>
2021-04-29 19:32:18 -0300

    network manager:

---
+++ desafio-ios-phi/desafio-ios-phi/Network/NetworkManager.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkManager.swift
+//  desafio-ios-phi
+//
+//  Created by Lidiane Gomes on 29/04/21.
+//
+
+import Foundation
+struct NetworkManager {
+    static func request(router: Router, completion: @escaping (Result<Data?, NetWorkResponseError>) -> Void) {
+        guard let urlRequest = router.urlRequest else {
+            completion(.failure(.malformedURLRequest(url: router.url?.absoluteString ?? "nil")))
+            return
+        }
+        
+        let session = URLSession(configuration: .default)
+      
+        let dataTask = session.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                completion(.failure(.requestFailed(description: error.localizedDescription)))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                return
+            }
+            
+            switch response.statusCode {
+            case 200, 201:
+                completion(.success(data))
+            case 400:
+                completion(.failure(.badRequest))
+            case 404:
+                completion(.failure(.notFound))
+            default:
+                completion(.failure(.unknownError(statusCode: response.statusCode)))
+            }
+            
+        }
+        
+        dataTask.resume()
+    }
+}
