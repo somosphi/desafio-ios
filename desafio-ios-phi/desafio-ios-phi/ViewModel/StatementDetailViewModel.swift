32870d45601cc663c6a32a9e9e60a5f4609a88c0
Lidiane Gomes <lidianegomes@c0387.local>
2021-04-30 18:01:25 -0300

    view models

--- desafio-ios-phi/desafio-ios-phi/ViewModel/StatementDetailViewModel.swift
+++ desafio-ios-phi/desafio-ios-phi/ViewModel/StatementDetailViewModel.swift
@@ -1,44 +1,43 @@
 //
-//  TransactionViewModel.swift
+//  StatementDetailViewModel.swift
 //  desafio-ios-phi
 //
 //  Created by Lidiane Gomes on 27/04/21.
 //
 
 import Foundation
-class TransactionViewModel {
-    private var transaction: Satatement?
+class StatementDetailViewModel {
+    
+    private var transaction: Statement?
+    private var transactionId: String
+    
     var uuid: UUID? {
         return UUID(uuidString: transaction?.uuid ?? "")
     }
-    var date: Date? {
-        return Date.formatFromString(transaction?.date ?? "")
+    
+    var date: String {
+        let date = Date.fromString(transaction?.date ?? "")
+        return String.fromDate(date ?? Date())
     }
+    
+    var dateResume: String {
+        let date = Date.fromString(transaction?.date ?? "")
+        return String.fromDateResume(date ?? Date())
+    }
+    
     var amount: Double {
         return transaction?.amount ?? 0
     }
+    
     var description: String {
         return transaction?.description ?? ""
     }
-    var typeOfBankTransaction: String {
-        let type = TransactionType(rawValue: transaction?.type ?? "") ?? .UNKNOWN
-        switch type {
-        case .TRANSFEROUT:
-            return "Transferência realizada"
-        case .TRANSFERIN:
-            return "Transferência recebida"
-        case .PIXCASHIN:
-            return "Transferência PIX recebida"
-        case .PIXCASHOUT:
-            return "Transferência PIX realizada"
-        case .BANKSLIPCASHIN:
-            return "Depósito via boleto"
-        default:
-              return "Transferência recebida"
-        }
-        
+    
+    var type: TransactionType? {
+       return TransactionType(rawValue: transaction?.type ?? "")
     }
-    var person: String {
+    
+    var userName: String {
         if let from = transaction?.from, transaction?.sentTo == nil {
             return from
         } else if let sentTo = transaction?.sentTo, transaction?.from == nil {
@@ -46,21 +45,34 @@ class TransactionViewModel {
         }
         return ""
     }
+    
     var bankName: String? {
         return transaction?.bankName
     }
+    
     var authentication: String {
         return transaction?.authentication ?? ""
     }
     
     init(transactionId: String) {
-        Service.getMyStatementDetail(transactionID: transactionId) { transaction in
-            guard let transaction = transaction else {
-                return
-            }
-            self.transaction = transaction
-        }
+        self.transactionId = transactionId
     }
     
-    
+    init(transaction: Statement) {
+        self.transaction = transaction
+        self.transactionId = transaction.uuid
+    }
+
+}
+
+extension StatementDetailViewModel {
+    func get(transactionId: String, completion :@escaping (StatementDetailViewModel) -> Void) {
+        Service.getMyStatementDetail(transactionID: transactionId) { transaction in
+            self.transaction = transaction
+            
+            DispatchQueue.main.async {
+                completion(self)
+            }
+        }
+    }
 }
--- desafio-ios-phi/desafio-ios-phi/ViewModel/StatementViewModel.swift
+++ desafio-ios-phi/desafio-ios-phi/ViewModel/StatementViewModel.swift
@@ -8,4 +8,44 @@
 import Foundation
 class StatementViewModel {
     
+    private var balance: Balance?
+    private var statement = [StatementDetailViewModel]()
+    
+    var amount: Double {
+        return balance?.amount ?? 0
+    }
+    
+    var numberOfTransactions: Int {
+        return statement.count
+    }
+    
+    func transaction(for index: Int) -> StatementDetailViewModel? {
+        if statement.count > index {
+            return statement[index]
+        }
+        return nil
+    }
+    
+    func updateBalance() {
+        Service.getMyBalance { balance in
+            self.balance = balance
+        }
+    }
+}
+
+extension StatementViewModel {
+    
+    func get(limit: Int = 10, offset: Int = 0, completion :@escaping (StatementViewModel) -> Void) {
+        Service.getMyBalance { balance in
+            self.balance = balance
+        }
+        
+        Service.getMyStatement(limit: limit, offset: offset) { statement in
+            self.statement = statement.map {StatementDetailViewModel(transaction: $0)}
+        }
+        
+        DispatchQueue.main.async {
+            completion(self)
+        }
+    }
 }
