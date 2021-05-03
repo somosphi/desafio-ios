7e35c9be9304128a05f82ce1db3ea29b4e7a20d8
Lidiane Gomes <lidianegomes@c0387.local>
2021-05-03 20:29:14 -0300

    header saldo

--- desafio-ios-phi/desafio-ios-phi/View/StatementViewController.swift
+++ desafio-ios-phi/desafio-ios-phi/View/StatementViewController.swift
@@ -8,30 +8,166 @@
 import UIKit
 
 class StatementViewController: UIViewController {
-    var startDetail: (() -> Void)?
     
-    let buttonTest: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
-        button.backgroundColor = .blue
-        return button
+    weak var coordinator: StatementCoordinator?
+    private var statementViewModel = StatementViewModel()
+    private var balanceHeaderView = BalanceHeaderView()
+    
+    private let loadingActivityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.style = .large
+        indicator.color = .black
+        indicator.startAnimating()
+        indicator.autoresizingMask = [
+            .flexibleLeftMargin, .flexibleRightMargin,
+            .flexibleTopMargin, .flexibleBottomMargin
+        ]
+        
+        return indicator
+    }()
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        tableView.layoutMargins = .zero
+        tableView.contentInset = .zero
+        return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupViewConfiguration()
+        loadMyBalance()
+        loadViewModel()
+    }
+    
+    private func loadMyBalance() {
+        self.statementViewModel.getMyBalance { statementViewModel in
+            self.statementViewModel = statementViewModel
+            DispatchQueue.main.async {
+                self.balanceHeaderView.updateAmount(statementViewModel.amount)
+                self.loadingActivityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    private func loadViewModel() {
+        self.statementViewModel.getStatement { statementViewModel in
+            self.statementViewModel = statementViewModel
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func configureNavigation() {
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationItem.title = "Extrato"
-       
-        buttonTest.addTarget(self, action: #selector(presentDetail), for: .touchUpInside)
-        view.addSubview(buttonTest)
-      
-        buttonTest.translatesAutoresizingMaskIntoConstraints = false
-        buttonTest.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        buttonTest.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
+    
+}
 
-    @objc func presentDetail() {
-        startDetail?()
+extension StatementViewController: ViewConfiguration {
+    
+    func configureViews() {
+        view.backgroundColor = .white
+        
+        loadingActivityIndicator.center = CGPoint(
+            x: view.bounds.midX,
+            y: view.bounds.midY
+        )
+        
+        configureNavigation()
+        configureTableView()
+    }
+    
+    func buildViewHierarchy() {
+        view.addSubview(tableView)
+        view.addSubview(loadingActivityIndicator)
+    }
+    
+    func setupConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+}
+
+extension StatementViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let transactionId = statementViewModel.getTransactionId(for: indexPath.row) else {
+            return
+        }
+        coordinator?.startStatementDetail(transactionId: transactionId)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        switch section {
+        case 0:
+            return balanceHeaderView
+        default:
+            return nil
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 100
+        default:
+            return 44
+        }
+    }
+    
+}
+
+extension StatementViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 1:
+            return 5
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.selectionStyle = .none
+        cell.insetsLayoutMarginsFromSafeArea = false
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 1:
+            return "Suas movimentações"
+        default:
+            return nil
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
     }
 }
