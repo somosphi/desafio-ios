5fc6991f438375848965090c1d04c8b7cdc252f8
Lidiane Gomes <lidianegomes@c0387.local>
2021-05-03 20:30:06 -0300

    load view model

--- desafio-ios-phi/desafio-ios-phi/View/StatementDetailViewController.swift
+++ desafio-ios-phi/desafio-ios-phi/View/StatementDetailViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class StatementDetailViewController: UIViewController {
-
+    var transactionId: String
+    var statementDetailViewModel: StatementDetailViewModel
+    
+    init(transactionId: String) {
+        self.transactionId = transactionId
+        statementDetailViewModel = StatementDetailViewModel(transactionId: transactionId)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        // Do any additional setup after loading the view.
+        loadViewModel()
     }
 
+    private func loadViewModel() {
+        self.statementDetailViewModel.get(transactionId: transactionId) { statementViewModel in
+            self.statementDetailViewModel = statementViewModel
+        }
+        
+    }
 }
