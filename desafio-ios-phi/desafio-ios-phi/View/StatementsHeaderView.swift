59d1f057d44a828afd8cd3eeadba7da5da280e68
Lidiane Gomes <lidianegomes@c0387.local>
2021-05-04 17:01:06 -0300

    header para a seção de statements

--- desafio-ios-phi/desafio-ios-phi/View/StatementsHeaderView.swift
+++ desafio-ios-phi/desafio-ios-phi/View/StatementsHeaderView.swift
@@ -5,4 +5,43 @@
 //  Created by Lidiane Gomes on 04/05/21.
 //
 
-import Foundation
+import UIKit
+class StatementsHeaderView: UIView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Suas movimentações"
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = .blackTextColor
+        return label
+    }()
+    
+    public init() {
+        super.init(frame: .zero)
+        setupViewConfiguration()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension StatementsHeaderView: ViewConfiguration {
+    func setupConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
+
+        ])
+    }
+    
+    func buildViewHierarchy() {
+        addSubview(titleLabel)
+    }
+    func configureViews() {
+        backgroundColor = .white
+    }
+    
+}
