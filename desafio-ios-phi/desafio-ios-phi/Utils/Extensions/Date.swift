30fb6a3d3fa444cddaa69f639d4f8d85382229e7
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-11 18:39:00 -0300

    converter Date para String

--- desafio-ios-phi/desafio-ios-phi/Utils/Extensions/Date.swift
+++ desafio-ios-phi/desafio-ios-phi/Utils/Extensions/Date.swift
@@ -5,4 +5,19 @@
 //  Created by Lidiane Gomes Barbosa on 11/05/21.
 //
 
-import Foundation
+import UIKit
+
+extension Date {
+   
+    var toString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/YYYY - HH:MM:ss"
+        return dateFormatter.string(from: self)
+    }
+    
+    var toShortString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM"
+        return dateFormatter.string(from: self)
+    }
+}
--- desafio-ios-phi/desafio-ios-phi/Utils/Extensions/UIImage.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIImage.swift
-//  desafio-ios-phi
-//
-//  Created by Lidiane Gomes Barbosa on 11/05/21.
-//
-
-import Foundation
