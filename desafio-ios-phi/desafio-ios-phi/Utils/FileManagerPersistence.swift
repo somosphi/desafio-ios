94ed40cb123c3e6ae2c5021f77a49468e8f8e532
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-11 18:39:29 -0300

    persistencia com File Manager

--- desafio-ios-phi/desafio-ios-phi/Utils/FileManagerPersistence.swift
+++ desafio-ios-phi/desafio-ios-phi/Utils/FileManagerPersistence.swift
@@ -1,5 +1,5 @@
 //
-//  FileManager.swift
+//  FileManagerPersistence.swift
 //  desafio-ios-phi
 //
 //  Created by Lidiane Gomes Barbosa on 11/05/21.
@@ -9,21 +9,31 @@ import UIKit
 
 class FileManagerPersistence {
     
-    let shared = FileManagerPersistence()
+    static let shared = FileManagerPersistence()
     
-    func getDocumentsDirectory() -> URL {
+    private func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
     
     func saveImage(image: UIImage, imageName: String) {
-        if let data = image.pngData() {
-            let filename = getDocumentsDirectory().appendingPathComponent(imageName)
-            try? data.write(to: filename)
+        if !fileExists(fileName: imageName) {
+            if let data = image.pngData() {
+                let filename = getDocumentsDirectory().appendingPathComponent(imageName)
+                try? data.write(to: filename)
+            }
         }
     }
     
-    func loadImage() {
-        
+    func getFileURL(fileName: String) -> URL? {
+        if fileExists(fileName: fileName) {
+            return getDocumentsDirectory().appendingPathComponent(fileName)
+        }
+        return nil
+    }
+    
+    private func fileExists(fileName: String) -> Bool {
+        let filename = getDocumentsDirectory().appendingPathComponent(fileName)
+        return FileManager.default.fileExists(atPath: filename.path)
     }
 }
