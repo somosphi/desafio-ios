192bf4ebbabc7f187bd2f0baa1a6f565750f8261
Lidiane Gomes <lidianegomes@c0387.local>
2021-05-18 15:49:09 -0300

    teste Date para um formato de String

--- desafio-ios-phi/desafio-ios-phiTests/Extensions/date_extension_tests.swift
+++ desafio-ios-phi/desafio-ios-phiTests/Extensions/date_extension_tests.swift
@@ -6,27 +6,27 @@
 //
 
 import XCTest
+@testable import desafio_ios_phi
 
-class date_extension_tests: XCTestCase {
+class DateExtensionTests: XCTestCase {
+    var dateToTest: Date!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        // "2021-05-18T15:05:31Z"
+        dateToTest = Date(timeIntervalSinceReferenceDate: 643055131)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        dateToTest = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_dateToString() {
+        let result = "18/05/2021 - 15:05:31"
+        XCTAssertEqual(dateToTest.toString, result)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_dateToShortString() {
+        let result = "18/05"
+        XCTAssertEqual(dateToTest.toShortString, result)
     }
-
 }
