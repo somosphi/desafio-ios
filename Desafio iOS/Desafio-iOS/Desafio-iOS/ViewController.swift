e5833c514ed245188af6997c3160e711cd2be838
Alex <otimizalex@outlook.com>
2021-05-20 11:17:12 +0100

    Ajustes no botao de exibir/esconder o saldo

--- Desafio iOS/Desafio-iOS/Desafio-iOS/ViewController.swift
+++ Desafio iOS/Desafio-iOS/Desafio-iOS/ViewController.swift
@@ -8,29 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var showHideButton: UIButton!
     @IBOutlet weak var amountActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
 
     var amountValue: Double = 0.0
     var favourites: [Int] = []
 
     override func viewDidLoad() {
 
-//        if let myImage = UIImage(named: "view-hide") {
-//            let tintableImage = myImage.withRenderingMode(.alwaysTemplate)
-//            showHideButton.setImage(tintableImage, for: .normal)
-//            // ver como mudar a cor da imagem de fundo do botao
-//            showHideButton.tintColor = UIColor.init(named: "phiGreen")
-//        }
+        // MARK: transformar numa função para atualizar quando recebe info da API
+        if let myImage = UIImage(named: "phiHide") {
+            let tintableImage = myImage.withRenderingMode(.alwaysTemplate)
+            showHideButton.setImage(tintableImage, for: .normal)
+            showHideButton.tintColor = UIColor.init(named: "phiGreen")
+        }
 
         self.amountLabel.isHidden = true
         self.amountActivityIndicator.startAnimating()
         self.amountActivityIndicator.isHidden = false
 
-        // teste de tint em imagem do sistema
-//        testImage.tintColor = UIColor.init(named: "phiGreen")
+        tableView.delegate = self
+        tableView.dataSource = self
 
         let service = QueryService()
 
@@ -61,12 +61,12 @@ class ViewController: UIViewController {
         func hideAmount() {
             if UserDefaults.standard.bool(forKey: "HideAmount") {
                 DispatchQueue.main.async {
-                    self.showHideButton.setImage(UIImage(named: "view-show"), for: .normal)
+                    self.showHideButton.setImage(UIImage(named: "phiShow"), for: .normal)
                     self.amountLabel.text = "💸💸💸"
                 }
             } else {
                 DispatchQueue.main.async {
-                    self.showHideButton.setImage(UIImage(named: "view-hide"), for: .normal)
+                    self.showHideButton.setImage(UIImage(named: "phiHide"), for: .normal)
                     self.amountLabel.text = String(format: "%.2f", self.amountValue)
                 }
             }
@@ -76,17 +76,32 @@ class ViewController: UIViewController {
 
     @IBAction func showHideButtonPressed(_ sender: Any) {
         if UserDefaults.standard.bool(forKey: "HideAmount") {
-            showHideButton.setImage(UIImage(named: "view-hide"), for: .normal)
+            showHideButton.setImage(UIImage(named: "phiHide"), for: .normal)
             UserDefaults.standard.setValue(false, forKey: "HideAmount")
             DispatchQueue.main.async {
                 self.amountLabel.text = String(format: "%.2f", self.amountValue)
             }
         } else {
-            showHideButton.setImage(UIImage(named: "view-show"), for: .normal)
+            showHideButton.setImage(UIImage(named: "phiShow"), for: .normal)
             UserDefaults.standard.setValue(true, forKey: "HideAmount")
             DispatchQueue.main.async {
                 self.amountLabel.text = "💸💸💸"
             }
         }
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StatementItem", for: indexPath)
+        return cell
     }
 }
