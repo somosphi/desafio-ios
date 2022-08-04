672f3c817842788fbb83ab7fbf057016b5737497
Suelen Critina Vaz <sucris12@gmail.com>
2022-08-04 12:46:24 -0300

    Create outlet the ExtractViewController and ExtractTableViewCell. Create file ExtractTableViewCell.

--- Desafio_Somosphi/Desafio_Somosphi/Extract/ExtractViewController.swift
+++ Desafio_Somosphi/Desafio_Somosphi/Extract/ExtractViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ExtractViewController: UIViewController {
 
+    @IBOutlet weak var textBalanceLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var movement: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
